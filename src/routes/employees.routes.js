9828bbb088dceaf9575d1a54b9c5626fd8c6d8d3
Interiano <josue.interiano88@gmail.com>
2023-04-21 22:52:28 -0600

    first commit

---
+++ src/routes/employees.routes.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import {getEmployees, createEmployee, updateEmployee, deleteEmployee, getEmployee} from '../controllers/employees.controller.js';
+const router = Router();
+
+router.get('/employees',getEmployees);
+router.get('/employee/:id',getEmployee);
+
+router.post('/employees',createEmployee);
+
+router.patch('/employees/:id',updateEmployee);
+
+//router.delete('/employees',deleteEmployee);
+router.delete('/employees/:id',deleteEmployee);
+export default router;
